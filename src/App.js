2641277c480ef797b9088c1dd3e6c478c85781ce
Joel Perez <joel_perez65@yahoo.com>
2023-03-04 00:25:46 -0600

    added user list component and useState to manage added users.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 import AddUser from './components/Users/AddUser';
+import UserList from './components/Users/UserList';
 
 function App() {
+  const [users, setUsers] = useState([])
+
+  const addUserHandler = (uName, uAge) => {
+    setUsers((prevUsersList) => {
+      return [...prevUsersList,
+      { name: uName, age: uAge, id: Math.random().toString() }]
+    })
+  }
+
   return (
     <div className="App">
-      <AddUser />
+      <AddUser onAddUser={addUserHandler} />
+      <UserList users={users} />
     </div>
   );
 }
--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,21 +1,43 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Button from '../UI/Button';
 import Card from '../UI/Card';
 import classes from "./AddUser.module.css";
 
 const AddUser = (props) => {
-  const addUserHandler = (e) => {
+  const [username, setUsername] = useState("")
+  const [age, setAge] = useState("")
+  const {onAddUser} = props
+
+  const addNewUserHandler = (e) => {
     e.preventDefault();
+    if (username.trim().length === 0 || age.trim().length === 0) {
+      return;
+    }
+    
+    if (+age < 1) {
+      return;
+    }
+    onAddUser(username, age);
+    setUsername("")
+    setAge("")
+  }
+
+  const usernameHandler = (e) => {
+    setUsername(e.target.value)
+  }
+
+  const ageHandler = (e) => {
+    setAge(e.target.value)
   }
 
   return (
     <Card className={classes.input}>
-      <form onSubmit={addUserHandler}>
+      <form onSubmit={addNewUserHandler}>
         <label htmlFor='username'>Username</label>
-        <input type="text" id="username" />
+        <input value={username} type="text" id="username" onChange={usernameHandler} />
         <label htmlFor='age'>Age (Years)</label>
-        <input type="number" id="age" />
-        <Button>Add User</Button>
+        <input value={age} type="number" id="age" onChange={ageHandler} />
+        <Button type="submit">Add User</Button>
       </form>
     </Card>
   )
