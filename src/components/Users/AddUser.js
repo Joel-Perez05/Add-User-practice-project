71dfb3314b6d1efac837236e299d4d0b3b0e7aba
Joel Perez <joel_perez65@yahoo.com>
2023-03-04 22:17:05 -0600

    added error modal with conditional rendering and error handling

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,20 +1,30 @@
 import React, {useState} from 'react'
 import Button from '../UI/Button';
 import Card from '../UI/Card';
+import ErrorModal from '../UI/ErrorModal';
 import classes from "./AddUser.module.css";
 
 const AddUser = (props) => {
   const [username, setUsername] = useState("")
   const [age, setAge] = useState("")
+  const [error, setError] = useState()
   const {onAddUser} = props
 
   const addNewUserHandler = (e) => {
     e.preventDefault();
     if (username.trim().length === 0 || age.trim().length === 0) {
+      setError({
+        title: "Invalid input",
+        message: "Please enter a valid name and age (non-empty values)."
+      })
       return;
     }
     
     if (+age < 1) {
+      setError({
+        title: "Invalid age",
+        message: "Please enter a valid age (> 0)."
+      })
       return;
     }
     onAddUser(username, age);
@@ -30,7 +40,13 @@ const AddUser = (props) => {
     setAge(e.target.value)
   }
 
+  const errorHandler = () => {
+    setError(null);
+  }
+
   return (
+    <div>
+    {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler} />}
     <Card className={classes.input}>
       <form onSubmit={addNewUserHandler}>
         <label htmlFor='username'>Username</label>
@@ -40,6 +56,7 @@ const AddUser = (props) => {
         <Button type="submit">Add User</Button>
       </form>
     </Card>
+    </div>  
   )
 }
 
